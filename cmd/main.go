842901e503064cf57e548dccc7252683d6aff897
mobamoh <b4m0h4m3d@gmail.com>
2024-05-19 23:09:06 +0200

    exploring htmx with golang

---
+++ cmd/main.go
@@ -0,0 +1,150 @@
+package main
+
+import (
+	"html/template"
+	"io"
+	"strconv"
+	"time"
+
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+)
+
+func main() {
+	e := echo.New()
+	e.Renderer = NewTemplates()
+	e.Use(middleware.Logger())
+	e.Static("/images", "images")
+	e.Static("/css", "css")
+
+	page := NewPage()
+	e.GET("/", func(c echo.Context) error {
+		return c.Render(200, "index", page)
+	})
+
+	e.POST("/contact", func(c echo.Context) error {
+		name := c.FormValue("name")
+		email := c.FormValue("name")
+		if page.Data.hasEmail(email) {
+			formData := NewFormData()
+			formData.Values["name"] = name
+			formData.Values["email"] = email
+			formData.Errors["email"] = "Email already exists"
+			return c.Render(422, "addContact", formData)
+		}
+
+		contact := NewContact(name, email)
+		page.Data.Contacts = append(page.Data.Contacts, contact)
+		c.Render(200, "addContact", NewFormData())
+		return c.Render(200, "oob-contact", contact)
+	})
+
+	e.DELETE("/contacts/:id", func(c echo.Context) error {
+		time.Sleep(3 * time.Second)
+
+		idStr := c.Param("id")
+		id, err := strconv.Atoi(idStr)
+		if err != nil {
+			return c.String(400, "Invalid id")
+		}
+
+		index := page.Data.indexOf(id)
+		if index == -1 {
+			return c.String(404, "Contact not found")
+		}
+
+		page.Data.Contacts = append(page.Data.Contacts[:index], page.Data.Contacts[index+1:]...)
+
+		return c.NoContent(200)
+	})
+
+	e.Logger.Fatal(e.Start(":1313"))
+}
+
+type Templates struct {
+	templates *template.Template
+}
+
+func (t *Templates) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
+	return t.templates.ExecuteTemplate(w, name, data)
+}
+
+func NewTemplates() *Templates {
+	return &Templates{
+		templates: template.Must(template.ParseGlob("views/*.html")),
+	}
+}
+
+var id = 0
+
+type Contact struct {
+	Name  string
+	Email string
+	Id    int
+}
+
+func NewContact(name, email string) Contact {
+	id++
+	return Contact{
+		Name:  name,
+		Email: email,
+		Id:    id,
+	}
+}
+
+type Contacts = []Contact
+
+type Data struct {
+	Contacts Contacts
+}
+
+func NewData() Data {
+	return Data{
+		[]Contact{
+			NewContact("mo", "mo"),
+			NewContact("ba", "ba"),
+		},
+	}
+}
+
+func (d *Data) hasEmail(email string) bool {
+	for _, c := range d.Contacts {
+		if c.Email == email {
+			return true
+		}
+	}
+	return false
+}
+
+func (d *Data) indexOf(id int) int {
+	for i, contact := range d.Contacts {
+		if contact.Id == id {
+			return i
+		}
+	}
+	return -1
+}
+
+type FormData struct {
+	Values map[string]string
+	Errors map[string]string
+}
+
+func NewFormData() FormData {
+	return FormData{
+		Values: make(map[string]string),
+		Errors: make(map[string]string),
+	}
+}
+
+type Page struct {
+	Data Data
+	Form FormData
+}
+
+func NewPage() Page {
+	return Page{
+		Data: NewData(),
+		Form: NewFormData(),
+	}
+}
